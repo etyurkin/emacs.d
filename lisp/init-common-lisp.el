3d586b674dc5226bacfc326c93d5977b1edfd08c
Evgeniy Tyurkin <etyurkin@kwarks.org>
2016-08-28 12:00:20 -0400

    fix startup errors; temporary use sbcl only

--- lisp/init-common-lisp.el
+++ lisp/init-common-lisp.el
@@ -5,16 +5,18 @@
                               (require 'slime)
                               (normal-mode))))
 
-(after-load 'slime
-  (when (executable-find "sbcl")
-    (add-to-list 'slime-lisp-implementations
-                 '(sbcl ("sbcl") :coding-system utf-8-unix)))
-  (when (executable-find "lisp")
-    (add-to-list 'slime-lisp-implementations
-                 '(cmucl ("lisp") :coding-system iso-latin-1-unix)))
-  (when (executable-find "ccl")
-    (add-to-list 'slime-lisp-implementations
-                 '(ccl ("ccl") :coding-system utf-8-unix))))
+;; (after-load 'slime
+;;   (when (executable-find "sbcl")
+;;     (add-to-list 'slime-lisp-implementations
+;;                  '(sbcl ("sbcl") :coding-system utf-8-unix)))
+;;   (when (executable-find "lisp")
+;;     (add-to-list 'slime-lisp-implementations
+;;                  '(cmucl ("lisp") :coding-system iso-latin-1-unix)))
+;;   (when (executable-find "ccl")
+;;     (add-to-list 'slime-lisp-implementations
+;;                  '(ccl ("ccl") :coding-system utf-8-unix))))
+
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
 
 ;; From http://bc.tech.coop/blog/070515.html
 (defun lispdoc ()
