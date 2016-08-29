1e5cffda0cd5f2e2741d5ad7d7cf87b245aadfcd
Evgeniy Tyurkin <evgeniy.tyurkin@oracle.com>
2016-08-29 16:28:40 -0400

    add powerline; disable startscreen

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -14,6 +14,35 @@
 
 (setq-default custom-enabled-themes '(doom-one))
 
+(use-package powerline :ensure t)
+
+;; Display date/time in mode line
+(setq display-time-day-and-date t)
+(setq display-time-string-forms
+      '((propertize (format-time-string "%a %b %d, %R" now) 'face 'bold)))
+
+(display-time-mode t)
+
+(setq powerline-display-hud nil)
+
+(custom-set-faces
+ '(mode-line ((t (:underline nil :foreground "light sea green" :background "#152F3F" :box nil))))
+ '(mode-line-inactive ((t (:underline nil))))
+ '(powerline-active1 ((t (:background "#154662" :foreground "light sea green"))))
+ '(powerline-active2 ((t (:background "#152F3F" :foreground "dark cyan"))))
+ '(powerline-arrow-shape (quote arrow)))
+
+(powerline-default-theme)
+
+(when (not (display-graphic-p))
+  (set-background-color "black")
+  (set-face-background 'hl-line "black")
+  (set-face-background 'mode-line "black")
+  (set-face-background 'mode-line-highlight "black")
+  (set-face-background 'powerline-inactive1 "black")
+  (set-face-background 'powerline-inactive2 "black")
+  (set-face-background 'linum "black"))
+
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
   "Forcibly load the themes listed in `custom-enabled-themes'."
