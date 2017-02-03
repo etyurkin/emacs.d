90160be23bddbfb82403273f41946b29fcb60cb7
Evgeniy Tyurkin <etyurkin@kwarks.org>
2017-02-02 20:14:49 -0500

    add multi-shell; new osx-updates function

--- lisp/init-osx.el
+++ lisp/init-osx.el
@@ -3,6 +3,8 @@
 ;;; Code:
 
 (when *is-a-mac*
+  (use-package osx-lib :ensure t)
+
   ;; Reset Option key from CMD back to Option
   ;;(setq mac-command-modifier 'meta)
   (setq mac-option-modifier 'meta)
@@ -14,11 +16,14 @@
   (dolist (multiple '("" "double-" "triple-"))
     (dolist (direction '("right" "left"))
       (global-set-key (read-kbd-macro (concat "<" multiple "wheel-" direction ">")) 'ignore)))
+
   (global-set-key (kbd "M-`") 'ns-next-frame)
   (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
   (global-set-key (kbd "M-˙") 'ns-do-hide-others)
+
   (after-load 'nxml-mode
     (define-key nxml-mode-map (kbd "M-h") nil))
+
   (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
 
   (defun osx-lock-screen ()
@@ -39,7 +44,24 @@
                    "/usr/bin/osascript" "-e"
                    (format "display notification \"%s\" with title \"%s\"" message title)))
 
-  (use-package osx-lib :ensure t)
+  ;; brew install mas
+  (defun osx-update-check ()
+    "Check for OSX updates."
+    (interactive)
+    (let ((buf "*osx-updates*"))
+      (start-process "osx-updates" buf "softwareupdate" "-l")
+      (start-process "osx-updates" buf "mas" "outdated")
+      (switch-to-buffer-other-window buf)))
+
+  (defun osx-update (password)
+    "Update OSX software."
+    (interactive (list (read-passwd "Sudo password for updates install: ")))
+    (let* ((buf "*osx-updates*")
+           (sys-proc (start-process "osx-system-updates" buf "sudo" "softwareupdate" "-ia" "--verbose"))
+           (usr-proc (start-process "osx-software-updates" buf "mas" "upgrade")))
+      (switch-to-buffer-other-window buf)
+      (process-send-string sys-proc (concat password "\r"))
+      (process-send-eof sys-proc)))
 
   (defun ns-raise-emacs ()
     "Raise Emacs."
