b19973b8f5d95a8aa898ad3d54ceb596e612defe
Evgeniy Tyurkin <evgeniy.tyurkin@oracle.com>
2017-05-17 10:39:56 -0400

    switch to literate config

--- lisp/init-flycheck.el
+++
@@ -1,7 +0,0 @@
-(use-package flycheck)
-
-(add-hook 'after-init-hook 'global-flycheck-mode)
-(setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
-
-
-(provide 'init-flycheck)
