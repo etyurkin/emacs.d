d3890f4a6915936ebfef446b89d0b2b478a29fb2
Evgeniy Tyurkin <etyurkin@kwarks.org>
2018-12-28 11:13:15 -0500

    upgrade packages

---
+++ elpa/lsp-javacomp-20181211.204/lsp-javacomp-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-javacomp" "20181211.204" "Provide Java IDE features powered by JavaComp." '((emacs "25.1") (lsp-mode "3.0") (s "1.2.0")) :stars '(#("16" 0 2 (font-lock-face paradox-star-face))) :commit "0cdf9c2e4e66aa77cfb23ed9d12c296cfe7db872" :keywords '("java" "tools" "lsp") :url "https://github.com/tigersoldier/lsp-javacomp")
