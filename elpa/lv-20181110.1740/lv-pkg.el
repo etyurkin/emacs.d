fde76354f520837e36c93d3e1d88d87901df7c17
Evgeniy Tyurkin <evgeniy.tyurkin@oracle.com>
2019-05-29 10:56:17 -0400

    packages upgrade

--- elpa/lv-20181110.1740/lv-pkg.el
+++ elpa/lv-20181110.1740/lv-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "lv" "20181110.1740" "Other echo area" 'nil :stars '(#("834" 0 3 (font-lock-face paradox-star-face))) :commit "9c4a4711bc78364e114acbbcf484a60669d02d78" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))
+(define-package "lv" "20181110.1740" "Other echo area" 'nil :commit "a2bb124a3f2e57c032fc264bcd08219fa3d513d9" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))
--- elpa/ztree-20180512.1850/ztree-autoloads.el
+++ elpa/ztree-20180512.1850/ztree-autoloads.el
@@ -12,6 +12,11 @@
 (autoload 'ztreediff-mode "ztree-diff" "\
 A minor mode for displaying the difference of the directory trees in text mode.
 
+If called interactively, enable ZtreeDiff mode if ARG is positive, and
+disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it
+if ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
 
 (autoload 'ztree-diff "ztree-diff" "\
