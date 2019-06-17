b73944acf3174429af1651768d7a485234986f6c
Evgeniy Tyurkin <evgeniy.tyurkin@oracle.com>
2019-06-17 15:38:42 -0400

    packages upgrade

--- elpa/counsel-20190527.1406/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20190527.1406" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :commit "7e8622f60487fc3a91540755103358a5cd599d12" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- elpa/swiper-20190528.1333/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190528.1333" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :commit "7e8622f60487fc3a91540755103358a5cd599d12" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
