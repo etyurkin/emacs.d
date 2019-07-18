20337e574362417484489a139f75c33d16708779
Evgeniy Tyurkin <evgeniy.tyurkin@oracle.com>
2019-07-18 10:36:08 -0400

    upgrade packages

--- elpa/ivy-rich-20190613.2345/ivy-rich-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/ivy-rich-20190613.2345/ivy-rich.el  -*- no-byte-compile: t -*-
-(define-package "ivy-rich" "20190613.2345" "More friendly display transformer for ivy." '((emacs "24.4") (ivy "0.8.0")) :stars '(#("135" 0 3 (font-lock-face paradox-star-face))) :commit "58da08aeff58267c5a982ebbfc5cce0ea66db107" :keywords '("ivy") :authors '(("Yevgnen Koh" . "wherejoystarts@gmail.com")) :maintainer '("Yevgnen Koh" . "wherejoystarts@gmail.com"))
