45ec5545a0828ddcb8e77aad228eaaed84194b9c
Evgeniy Tyurkin <evgeniy.tyurkin@oracle.com>
2019-08-13 15:08:21 -0400

    upgrade packages

--- elpa/counsel-20190716.1748/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/counsel-20190716.1748/counsel.el  -*- no-byte-compile: t -*-
-(define-package "counsel" "20190716.1748" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "54d1b197080c78320ee611d5ed97ee8a34cebe0a" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
