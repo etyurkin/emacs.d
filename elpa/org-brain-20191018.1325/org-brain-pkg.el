35b367be996f5c24c833dd93e2b7f84f7447398c
Evgeniy Tyurkin <eugenet@ithink.info>
2019-10-22 11:50:41 -0400

    upgrade packages

---
+++ elpa/org-brain-20191018.1325/org-brain-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-brain" "20191018.1325" "Org-mode concept mapping" '((emacs "25.1") (org "9.2")) :stars '(#("896" 0 3 (font-lock-face paradox-star-face))) :commit "715fa33f96b57b7cc9dbda7f5a760c0d3a5089a5" :keywords '("outlines" "hypermedia") :authors '(("Erik Sjöstrand" . "sjostrand.erik@gmail.com")) :maintainer '("Erik Sjöstrand" . "sjostrand.erik@gmail.com") :url "http://github.com/Kungsgeten/org-brain")
