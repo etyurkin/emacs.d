9bfa15843c14478253b3da2b59221b230562a771
Evgeniy Tyurkin <eugenet@ithink.info>
2019-10-22 11:50:41 -0400

    upgrade packages

--- elpa/lv-20190821.947/lv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/lv-20190821.947/lv.el  -*- no-byte-compile: t -*-
-(define-package "lv" "20190821.947" "Other echo area" 'nil :stars '(#("999" 0 3 (font-lock-face paradox-star-face))) :commit "435c55e9f75a8cf3ae6a4ba0c7725e3dc4e5963f" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))
--- elpa/org-brain-20190922.1414/org-brain-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-brain" "20190922.1414" "Org-mode concept mapping" '((emacs "25") (org "9.2")) :stars '(#("896" 0 3 (font-lock-face paradox-star-face))) :commit "1f86e92c72cf52b75695c99572eeace7405caf96" :keywords '("outlines" "hypermedia") :authors '(("Erik Sjöstrand" . "sjostrand.erik@gmail.com")) :maintainer '("Erik Sjöstrand" . "sjostrand.erik@gmail.com") :url "http://github.com/Kungsgeten/org-brain")
