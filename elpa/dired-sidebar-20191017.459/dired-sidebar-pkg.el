893f259e3e89d2b356e3b867e166f50101415958
Evgeniy Tyurkin <eugenet@ithink.info>
2019-11-12 10:14:12 -0500

    upgrade packages; fix modeline

--- elpa/dired-sidebar-20191017.459/dired-sidebar-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dired-sidebar" "20191017.459" "Tree browser leveraging dired" '((emacs "25.1") (dired-subtree "0.0.1")) :stars '(#("191" 0 3 (font-lock-face paradox-star-face))) :commit "347f56480228c2aac97e14f4f5a762c4582d1323" :keywords '("dired" "files" "tools") :authors '(("James Nguyen" . "james@jojojames.com")) :maintainer '("James Nguyen" . "james@jojojames.com") :url "https://github.com/jojojames/dired-sidebar")
--- elpa/ivy-rich-20190707.107/ivy-rich-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/ivy-rich-20190707.107/ivy-rich.el  -*- no-byte-compile: t -*-
-(define-package "ivy-rich" "20190707.107" "More friendly display transformer for ivy." '((emacs "24.4") (ivy "0.8.0")) :stars '(#("135" 0 3 (font-lock-face paradox-star-face))) :commit "74297149c64451ef26864e936a4c89944c42c4bc" :keywords '("ivy") :authors '(("Yevgnen Koh" . "wherejoystarts@gmail.com")) :maintainer '("Yevgnen Koh" . "wherejoystarts@gmail.com"))
