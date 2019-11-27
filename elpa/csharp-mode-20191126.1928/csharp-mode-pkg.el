728961adb8e7851dcf826a83c5a034d6e989e9a7
Evgeniy Tyurkin <eugenet@ithink.info>
2019-11-27 03:42:02 +0000

    upgrade packages

---
+++ elpa/csharp-mode-20191126.1928/csharp-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "csharp-mode" "20191126.1928" "C# mode derived mode" 'nil :stars '(#("94" 0 2 (font-lock-face paradox-star-face))) :commit "57bd21bda4edc16671a85c7d6d51484e40a6e640" :keywords '("c#" "languages" "oop" "mode") :authors '(("Dylan R. E. Moonfire (original)")) :maintainer '("Jostein Kjønigsen" . "jostein@gmail.com") :url "https://github.com/josteink/csharp-mode")
