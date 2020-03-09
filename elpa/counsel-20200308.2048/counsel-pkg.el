aaa9b35b3a22e28ed82771e1f4ea74a5c7f8c9b6
Evgeniy Tyurkin <etyurkin@Evgeniys-MacBook-Air.local>
2020-03-09 13:57:02 -0400

    upgrade packages

---
+++ elpa/counsel-20200308.2048/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20200308.2048" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "328da1f95ce5f3386b63b28ca41a48bd88cf4144" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
