7c52a61e71a88fc0f227639928ac81842f4363ce
Evgeniy Tyurkin <eugenet@ithink.info>
2020-03-19 10:15:15 -0400

    upgrade packages

--- elpa/reverse-im-20200219.1137/reverse-im-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "reverse-im" "20200219.1137" "Reverse mapping for non-default system layouts" '((emacs "25.1")) :stars '(#("30" 0 2 (font-lock-face paradox-star-face))) :commit "45a55810ce3e07d4e42d9fb666100d7242004b4e" :keywords '("i18n") :url "https://github.com/a13/reverse-im.el")
---
+++ elpa/reverse-im-20200319.1220/reverse-im-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "reverse-im" "20200319.1220" "Reverse mapping for non-default system layouts" '((emacs "25.1")) :stars '(#("30" 0 2 (font-lock-face paradox-star-face))) :commit "ba8da7d5eed5dff6c79809cc34d1212a932e43f7" :keywords '("i18n") :url "https://github.com/a13/reverse-im.el")
