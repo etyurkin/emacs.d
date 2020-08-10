2d1847d7560dae65adce51c9ffd69cca8a89bcf7
evgeniy_tyurkin <eugenet@ithink.info>
2020-08-10 13:06:21 -0400

    upgrade packages

---
+++ elpa/annot-20160315.703/annot-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "annot" "20160315.703" "a global annotator/highlighter for GNU Emacs" 'nil :authors '(("tkykhs")) :maintainer '("tkykhs"))
--- elpa/xterm-color-20200501.1723/xterm-color-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "xterm-color" "20200501.1723" "ANSI, XTERM 256 and Truecolor support" '((emacs "24.4")) :stars '(#("102" 0 3 (font-lock-face paradox-star-face))) :commit "d53a39a5af72cd340ebf686e59a37289b4cb6e8c" :keywords '("faces") :authors '(("xristos" . "xristos@sdf.org")) :maintainer '("xristos" . "xristos@sdf.org") :url "https://github.com/atomontage/xterm-color")
