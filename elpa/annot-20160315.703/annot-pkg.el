b4d5301607ae391359b6934de528c8d9c6848b37
evgeniy_tyurkin <eugenet@ithink.info>
2020-08-13 11:29:16 -0400

    upgrade packages

--- elpa/annot-20160315.703/annot-pkg.el
+++ elpa/annot-20160315.703/annot-pkg.el
@@ -1,2 +1,2 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "annot" "20160315.703" "a global annotator/highlighter for GNU Emacs" 'nil :authors '(("tkykhs")) :maintainer '("tkykhs"))
+;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/annot-20160315.703/annot.el  -*- no-byte-compile: t -*-
+(define-package "annot" "20160315.703" "a global annotator/highlighter for GNU Emacs" 'nil :authors '(("tkykhs")) :maintainer '("tkykhs") :keywords '("convenience" "frames" "local" "multimedia" "terminals" "tools"))
--- elpa/paradox-20191011.1111/paradox-pkg.el
+++ elpa/paradox-20191011.1111/paradox-pkg.el
@@ -4,7 +4,7 @@
     (let-alist "1.0.3")
     (spinner "1.7.3")
     (hydra "0.13.2"))
-  :keywords
+  :commit "339fe3518d1d102b2295670340e75caf4f01a29a" :keywords
   '("package" "packages")
   :authors
   '(("Artur Malabarba" . "emacs@endlessparentheses.com"))
