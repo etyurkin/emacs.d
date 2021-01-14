27de4bc16d8a22b768f78315e84538d05bc4969b
evgeniy_tyurkin <eugenet@ithink.info>
2021-01-14 16:57:41 -0500

    upgrade packages

---
+++ elpa/markdown-mode-20210112.1557/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/markdown-mode-20210112.1557/markdown-mode.el  -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20210112.1557" "Major mode for Markdown-formatted text" '((emacs "25.1")) :stars '(#("443" 0 3 (font-lock-face paradox-star-face))) :commit "3e380572a5177d0fa527c15efd15e340f53a923b" :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :keywords '("markdown" "github flavored markdown" "itex") :url "https://jblevins.org/projects/markdown-mode/")
