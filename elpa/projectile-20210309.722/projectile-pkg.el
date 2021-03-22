c1b279160a2bfd39e25b8a4beae2c1db7db03a7e
evgeniy_tyurkin <eugenet@ithink.info>
2021-03-22 10:30:36 -0400

    upgrade packages

---
+++ elpa/projectile-20210309.722/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/projectile-20210309.722/projectile.el  -*- no-byte-compile: t -*-
+(define-package "projectile" "20210309.722" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :stars '(#("2614" 0 4 (font-lock-face paradox-star-face))) :commit "1528ed4f082e7aaca19f22394eb4bed879645b7c" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :keywords '("project" "convenience") :url "https://github.com/bbatsov/projectile")
