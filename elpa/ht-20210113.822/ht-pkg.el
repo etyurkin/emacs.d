9354574a8dacd9bf170341e5b651c973250ebe82
Evgeniy Tyurkin <etyurkin@kwarks.org>
2021-03-22 10:30:36 -0400

    upgrade packages

--- elpa/ht-20210113.822/ht-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/ht-20210113.822/ht.el  -*- no-byte-compile: t -*-
-(define-package "ht" "20210113.822" "The missing hash table library for Emacs" '((dash "2.12.0")) :stars '(#("137" 0 3 (font-lock-face paradox-star-face))) :commit "5876209c9712bd1acbbf1d31f6e3293e5ab88971" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk") :keywords '("hash table" "hash map" "hash"))
