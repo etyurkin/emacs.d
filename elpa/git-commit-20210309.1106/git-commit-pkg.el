a6b614f664e7d605373efae6e457755452f62c24
evgeniy_tyurkin <eugenet@ithink.info>
2021-05-07 13:31:44 -0400

    upgrade packages

--- elpa/git-commit-20210309.1106/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/git-commit-20210309.1106/git-commit.el  -*- no-byte-compile: t -*-
-(define-package "git-commit" "20210309.1106" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :stars '(#("3932" 0 4 (font-lock-face paradox-star-face))) :commit "4735b9254105eb7dd538f979d8b4c6ab96b827b9" :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
--- elpa/swiper-20210310.1230/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/etyurkin/.emacs.d/elpa/swiper-20210310.1230/swiper.el  -*- no-byte-compile: t -*-
-(define-package "swiper" "20210310.1230" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.4")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face))) :commit "fb1ef155578b4642e37b9856f1f9e3ba1f90a367" :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :keywords '("matching") :url "https://github.com/abo-abo/swiper")
