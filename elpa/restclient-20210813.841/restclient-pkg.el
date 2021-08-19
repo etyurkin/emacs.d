87eaafc8748c10f28c5524fb84c9a2b1a16adf1c
evgeniy_tyurkin <eugenet@ithink.info>
2021-08-19 11:00:14 -0400

    packages upgrade

---
+++ elpa/restclient-20210813.841/restclient-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from restclient.el  -*- no-byte-compile: t -*-
+(define-package "restclient" "20210813.841" "An interactive HTTP client for Emacs" 'nil :stars '(#("1179" 0 4 (font-lock-face paradox-star-face))) :commit "176d9cb6552f04d98c33e29fc673862bdf3bca03" :authors '(("Pavel Kurnosov" . "pashky@gmail.com")) :maintainer '("Pavel Kurnosov" . "pashky@gmail.com") :keywords '("http"))
