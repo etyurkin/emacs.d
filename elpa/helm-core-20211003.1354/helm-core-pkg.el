199aaddf1ceef17b9b065e02186bdc6344510e9f
evgeniy_tyurkin <eugenet@ithink.info>
2021-10-04 10:44:58 -0400

    upgrade packages

---
+++ elpa/helm-core-20211003.1354/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20211003.1354" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "fa60663b58f028d7b084626ee1d88c1470d8efc1")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
