c73a72b3a91c3fcfdb69e6379a449b0aed7248ff
Evgeniy Tyurkin <etyurkin@kwark.local>
2022-01-16 22:51:08 -0500

    upgrade packages

--- elpa/org-passwords-20190102.54/org-passwords-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from org-passwords.el  -*- no-byte-compile: t -*-
-(define-package "org-passwords" "20190102.54" "org derived mode for managing passwords" 'nil :authors '(("Jorge A. Alfaro-Murillo" . "jorge.alfaro-murillo@yale.edu")) :maintainer '("Jorge A. Alfaro-Murillo" . "jorge.alfaro-murillo@yale.edu") :keywords '("passwords" "password"))
