658852f788606186616314f126cd725ba8df7ddc
Evgeniy Tyurkin <etyurkin@kwark.local>
2022-01-31 12:34:27 -0500

    upgrade packages

---
+++ elpa/geiser-20220128.2258/geiser-pkg.el
@@ -0,0 +1,13 @@
+(define-package "geiser" "20220128.2258" "GNU Emacs and Scheme talk to each other"
+  '((emacs "25.1")
+    (transient "0.3"))
+  :commit "22702313a1f65abaa3bb691cc82cb50b1c14f455" :authors
+  '(("Jose Antonio Ortega Ruiz" . "jao@gnu.org"))
+  :maintainer
+  '("Jose Antonio Ortega Ruiz" . "jao@gnu.org")
+  :keywords
+  '("languages" "scheme" "geiser")
+  :url "https://gitlab.com/emacs-geiser/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/racket-mode-20220129.1749/racket-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "racket-mode" "20220129.1749" "Racket editing, REPL, and more"
+  '((emacs "25.1"))
+  :commit "3027de086eb0b77ac8ae81acceeddab91c66ba0e" :authors
+  '(("Greg Hendershott"))
+  :maintainer
+  '("Greg Hendershott")
+  :url "https://www.racket-mode.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
