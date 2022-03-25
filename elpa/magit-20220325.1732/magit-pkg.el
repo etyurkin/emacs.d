a79ca41cbf93041c474bd36534acb5cde092c2f9
Evgeniy Tyurkin <etyurkin@kwarks.org>
2022-03-25 15:11:11 -0400

    upgrade packages; require helm instead of helm-core because of org-wiki dependency

---
+++ elpa/magit-20220325.1732/magit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "magit" "20220325.1732" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20210826")
+    (git-commit "20220222")
+    (magit-section "20220325")
+    (transient "20220325")
+    (with-editor "20220318"))
+  :commit "f3afc117c236972901dc46717497b87004a23bbb" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
