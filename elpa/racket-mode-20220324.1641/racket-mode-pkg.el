3ec9613dc0d49d8b8a8bfb997413ac0e8c595052
Evgeniy Tyurkin <etyurkin@kwark.local>
2022-03-25 15:11:11 -0400

    upgrade packages; require helm instead of helm-core because of org-wiki dependency

---
+++ elpa/racket-mode-20220324.1641/racket-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "racket-mode" "20220324.1641" "Racket editing, REPL, and more"
+  '((emacs "25.1"))
+  :commit "45baad2f5217cbe5ad53f1b0f99a496b20035218" :authors
+  '(("Greg Hendershott"))
+  :maintainer
+  '("Greg Hendershott")
+  :url "https://www.racket-mode.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/scratch-20220319.1705/scratch-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from scratch.el  -*- no-byte-compile: t -*-
+(define-package "scratch" "20220319.1705" "Mode-specific scratch buffers" '((emacs "25.1")) :stars '(#("40" 0 2 (font-lock-face paradox-star-face))) :commit "f000648c9663833a76a8de9b1e78c99a9d698e48" :authors '(("Ian Eure" . "ian.eure@gmail.com")) :maintainer '("Ian Eure" . "ian.eure@gmail.com") :keywords '("convenience" "tools" "files") :url "https://github.com/ieure/scratch-el")
