6a71c8d3de1ee24057e349b934232bb249995d14
Evgeniy Tyurkin <etyurkin@kwarks.org>
2022-09-20 13:41:35 -0400

    upgrade packages

---
+++ elpa/racket-mode-20220912.1749/racket-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "racket-mode" "20220912.1749" "Racket editing, REPL, and more"
+  '((emacs "25.1"))
+  :commit "caa9259ad08f3aa7b9fb4441933ca3ac7254fad2" :authors
+  '(("Greg Hendershott"))
+  :maintainer
+  '("Greg Hendershott")
+  :url "https://www.racket-mode.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
