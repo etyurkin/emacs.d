73bf2359b0562e329109cb1e660c423cb890ccd8
Evgeniy Tyurkin <etyurkin@icloud.com>
2022-10-31 10:57:08 -0400

    upgrade packages

---
+++ elpa/focus-20221016.1846/focus-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from focus.el  -*- no-byte-compile: t -*-
+(define-package "focus" "20221016.1846" "Dim the font color of text in surrounding sections" '((emacs "24.3") (cl-lib "0.5")) :stars '(#("205" 0 3 (font-lock-face paradox-star-face))) :commit "2507ec4ec5a9402647ef85540669db1815520c15" :authors '(("Lars Tveito" . "larstvei@ifi.uio.no")) :maintainer '("Lars Tveito" . "larstvei@ifi.uio.no") :url "http://github.com/larstvei/Focus")
