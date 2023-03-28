294836910d0eb1b7d388dfe7ea29c996c324198c
Evgeniy Tyurkin <etyurkin@icloud.com>
2023-03-28 17:13:12 -0400

    upgrade packages

---
+++ elpa/pcmpl-args-20220510.2056/pcmpl-args-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from pcmpl-args.el  -*- no-byte-compile: t -*-
+(define-package "pcmpl-args" "20220510.2056" "Enhanced shell command completion" '((emacs "25.1")) :commit "43229e1096f89c277190f09a3d794781f8fa0015" :authors '(("Jonathan Waltman" . "jonathan.waltman@gmail.com")) :maintainer '("Jonathan Waltman" . "jonathan.waltman@gmail.com") :keywords '("abbrev" "completion" "convenience" "processes" "terminals" "unix") :url "https://github.com/JonWaltman/pcmpl-args.el")
