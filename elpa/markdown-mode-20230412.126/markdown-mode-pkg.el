80b3c7282ef8713d732e80949d0b5222b47e719d
Evgeniy Tyurkin <etyurkin@icloud.com>
2023-04-14 14:32:21 -0400

    upgrade packages

---
+++ elpa/markdown-mode-20230412.126/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from markdown-mode.el  -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20230412.126" "Major mode for Markdown-formatted text" '((emacs "26.1")) :stars '(#("443" 0 3 (font-lock-face paradox-star-face))) :commit "5d98592fe516748034d8baf92d7c0ba045e1f87a" :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :keywords '("markdown" "github flavored markdown" "itex") :url "https://jblevins.org/projects/markdown-mode/")
