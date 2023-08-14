96018d44b9655eef8027b8b7841dc71daf1540b8
Evgeniy Tyurkin <etyurkin@kwarks.org>
2023-08-14 11:07:58 -0400

    remove depricated linum; upgrade packages

---
+++ elpa/native-complete-20230505.1803/native-complete-pkg.el
@@ -0,0 +1,12 @@
+(define-package "native-complete" "20230505.1803" "Shell completion using native complete mechanisms"
+  '((emacs "26.1"))
+  :commit "7b5e7d86c39ce9833118db278034789a6c0ecfd6" :authors
+  '(("Troy Hinckley" . "troy.hinckley@gmail.com"))
+  :maintainers
+  '(("Troy Hinckley" . "troy.hinckley@gmail.com"))
+  :maintainer
+  '("Troy Hinckley" . "troy.hinckley@gmail.com")
+  :url "https://github.com/CeleritasCelery/emacs-native-shell-complete")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
