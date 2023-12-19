c3a544a9ef3ccc7a85e31825378c8f1ed4a3f922
Evgeniy Tyurkin <evgeniy.tyurkin@virtualintelligencebriefing.com>
2023-12-19 00:41:59 -0500

    upgrade packages

---
+++ elpa/ob-restclient-20231027.518/ob-restclient-pkg.el
@@ -0,0 +1,14 @@
+(define-package "ob-restclient" "20231027.518" "org-babel functions for restclient-mode"
+  '((restclient "0"))
+  :commit "1a127eb0165f10bb9d33606aa8529051118805e7" :authors
+  '(("Alf Lervåg"))
+  :maintainers
+  '(("Alf Lervåg"))
+  :maintainer
+  '("Alf Lervåg")
+  :keywords
+  '("literate programming" "reproducible research")
+  :url "https://github.com/alf/ob-restclient.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
