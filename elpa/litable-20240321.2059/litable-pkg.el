56e0d698a66afb89ccd41929134c125b69679976
Evgeniy Tyurkin <evgeniy.tyurkin@virtualintelligencebriefing.com>
2024-06-18 16:37:11 -0400

    upgrade packages

---
+++ elpa/litable-20240321.2059/litable-pkg.el
@@ -0,0 +1,13 @@
+(define-package "litable" "20240321.2059" "dynamic evaluation replacement with emacs"
+  '((dash "2.6.0"))
+  :commit "b83b1283ea6642ab82f536f1f3b280160404ff6b" :authors
+  '(("Matus Goljer" . "matus.goljer@gmail.com"))
+  :maintainers
+  '(("Matus Goljer" . "matus.goljer@gmail.com"))
+  :maintainer
+  '("Matus Goljer" . "matus.goljer@gmail.com")
+  :keywords
+  '("lisp"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
