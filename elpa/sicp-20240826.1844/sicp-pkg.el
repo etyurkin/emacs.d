0badb967a280b816aabd5363bc562f3bbb667b16
Evgeniy Tyurkin <etyurkin@kwarks.org>
2024-09-01 09:40:33 -0400

    upgrade packages

---
+++ elpa/sicp-20240826.1844/sicp-pkg.el
@@ -0,0 +1,4 @@
+(define-package "sicp" "20240826.1844" "Structure and Interpretation of Computer Programs in info format" 'nil :commit "552b44fd873b5cadde4e76d10cef4d6e21a4287a" :url "https://mitpress.mit.edu/sicp")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
