2c41802b26510328ec2fa5674b145a55afb14950
Evgeniy Tyurkin <etyurkin@kwark.local>
2024-09-01 09:40:33 -0400

    upgrade packages

---
+++ elpa/magit-section-20240830.1753/magit-section-pkg.el
@@ -0,0 +1,17 @@
+(define-package "magit-section" "20240830.1753" "Sections for read-only buffers"
+  '((emacs "26.1")
+    (compat "30.0.0.0")
+    (dash "2.19.1")
+    (seq "2.24"))
+  :commit "2996e2f2e9bfb017ee7520d67be22c902eea9c7f" :authors
+  '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers
+  '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
