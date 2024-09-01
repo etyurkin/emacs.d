0798023f614eb87db1fcdced4d29afd930e7c21d
Evgeniy Tyurkin <etyurkin@kwark.local>
2024-09-01 09:40:33 -0400

    upgrade packages

---
+++ elpa/reverse-im-20240815.1055/reverse-im-pkg.el
@@ -0,0 +1,15 @@
+(define-package "reverse-im" "20240815.1055" "Reverse mapping for non-default system layouts"
+  '((emacs "25.1")
+    (seq "2.23"))
+  :commit "dfb169b08e09be6ab113a7d423f79b1775a9334f" :authors
+  '(("Juri Linkov" . "juri@jurta.org"))
+  :maintainers
+  '(("DK" . "a13@users.noreply.github.com"))
+  :maintainer
+  '("DK" . "a13@users.noreply.github.com")
+  :keywords
+  '("i18n")
+  :url "https://github.com/a13/reverse-im.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
