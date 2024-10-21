edd557fb19a86f37a234b66ac0e64c340162c887
Evgeniy Tyurkin <etyurkin@kwark.local>
2024-10-21 10:30:15 -0400

    add AI assistant (ellama)

---
+++ elpa/llm-0.17.4/llm-autoloads.el
@@ -0,0 +1,131 @@
+;;; llm-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "llm" "llm.el" (0 0 0 0))
+;;; Generated autoloads from llm.el
+
+(register-definition-prefixes "llm" '("llm-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-claude" "llm-claude.el" (0 0 0 0))
+;;; Generated autoloads from llm-claude.el
+
+(register-definition-prefixes "llm-claude" '("llm-claude--tool-call"))
+
+;;;***
+
+;;;### (autoloads nil "llm-gemini" "llm-gemini.el" (0 0 0 0))
+;;; Generated autoloads from llm-gemini.el
+
+(register-definition-prefixes "llm-gemini" '("llm-gemini--chat-url"))
+
+;;;***
+
+;;;### (autoloads nil "llm-integration-test" "llm-integration-test.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from llm-integration-test.el
+
+(register-definition-prefixes "llm-integration-test" '("llm-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-llamacpp" "llm-llamacpp.el" (0 0 0 0))
+;;; Generated autoloads from llm-llamacpp.el
+
+(register-definition-prefixes "llm-llamacpp" '("llm-llamacpp-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-ollama" "llm-ollama.el" (0 0 0 0))
+;;; Generated autoloads from llm-ollama.el
+
+(register-definition-prefixes "llm-ollama" '("llm-ollama-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-openai" "llm-openai.el" (0 0 0 0))
+;;; Generated autoloads from llm-openai.el
+
+(register-definition-prefixes "llm-openai" '("llm-openai-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-prompt" "llm-prompt.el" (0 0 0 0))
+;;; Generated autoloads from llm-prompt.el
+
+(register-definition-prefixes "llm-prompt" '("llm-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-prompt-test" "llm-prompt-test.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from llm-prompt-test.el
+
+(register-definition-prefixes "llm-prompt-test" '("approx-equal"))
+
+;;;***
+
+;;;### (autoloads nil "llm-provider-utils" "llm-provider-utils.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from llm-provider-utils.el
+
+(register-definition-prefixes "llm-provider-utils" '("llm-provider-utils-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-request-plz" "llm-request-plz.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from llm-request-plz.el
+
+(register-definition-prefixes "llm-request-plz" '("llm-request-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-tester" "llm-tester.el" (0 0 0 0))
+;;; Generated autoloads from llm-tester.el
+
+(register-definition-prefixes "llm-tester" '("llm-tester-"))
+
+;;;***
+
+;;;### (autoloads nil "llm-vertex" "llm-vertex.el" (0 0 0 0))
+;;; Generated autoloads from llm-vertex.el
+
+(register-definition-prefixes "llm-vertex" '("llm-vertex-"))
+
+;;;***
+
+;;;### (autoloads nil "plz-event-source" "plz-event-source.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from plz-event-source.el
+
+(register-definition-prefixes "plz-event-source" '("plz-event-source"))
+
+;;;***
+
+;;;### (autoloads nil "plz-media-type" "plz-media-type.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from plz-media-type.el
+
+(register-definition-prefixes "plz-media-type" '("plz-media-type"))
+
+;;;***
+
+;;;### (autoloads nil nil ("llm-fake.el" "llm-gpt4all.el" "llm-pkg.el"
+;;;;;;  "llm-provider-utils-test.el" "llm-test.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; llm-autoloads.el ends here
